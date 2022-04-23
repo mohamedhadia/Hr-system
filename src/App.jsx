08545ceea37fe2655ea1cb4551e7bfb4e999530a
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-23 09:26:46 +0200

    redirect user to login to signin first before access admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
 import Test from "./Dashboard/Test";
 import Admin from "./Dashboard/Admin";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user);
+  const location = useLocation();
+
   return (
     <Routes>
       <Route path="/" element={<Test />} />
-      <Route path="/admin/*" element={<Admin />} />
+      {/* <Route path="/admin/*" element={<Admin />} /> */}
+
+      <Route
+        path="/admin/*"
+        element={
+          !user.isLoggedin ? (
+            <Navigate to="/" replace state={{ from: location }} />
+          ) : (
+            <Admin />
+          )
+        }
+      />
+
       <Route
         path="*"
         element={
--- src/Dashboard/Employees2.jsx
+++ src/Dashboard/Employees2.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Popup from "./Popup";
 
 const Employees2 = () => {
   return <div className=" text-black">Another empty Page</div>;
