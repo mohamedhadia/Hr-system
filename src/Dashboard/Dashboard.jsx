ec6d4a668f7f84b3950a74e28515f5e424252810
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-23 08:58:54 +0200

    move logout button to dashboard

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -5,16 +5,17 @@ import { logout } from "./../features/login/loginSlice";
 import Table from "./table/Table";
 import { UsersIcon } from "@heroicons/react/solid";
 import Employees from "./Employees";
+import { Navigate } from "react-big-calendar";
 
 const Dashboard = (props) => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  let navigate = useNavigate();
+  let Navigate = useNavigate();
 
   return (
     <div className="h-full w-full bg-[#fff]">
       <div className="flex flex-col md:h-screen md:flex-row ">
-        <div className="flex items-center justify-around gap-10 bg-[#dceef3] py-4 text-[#111111] md:fixed md:h-screen md:w-2/6 md:flex-col md:justify-start lg:w-1/5 ">
+        <div className="flex flex-col items-center justify-around gap-10 bg-[#dceef3] py-4 text-[#111111] md:fixed md:h-screen md:w-2/6 md:justify-start lg:w-1/5 ">
           <Link
             to="/"
             className="text-lg font-bold md:py-8 md:text-center md:text-4xl"
@@ -44,17 +45,16 @@ const Dashboard = (props) => {
               <UsersIcon className="h-5 w-5  text-gray-600 " />
               employees2
             </NavLink>
-
-            <button
-              className="btn mt-4 "
-              onClick={() => {
-                dispatch(logout());
-                Navigate("/");
-              }}
-            >
-              logout
-            </button>
           </div>
+          <button
+            className="btn mt-4 "
+            onClick={() => {
+              dispatch(logout());
+              Navigate("/");
+            }}
+          >
+            logout
+          </button>
         </div>
         <div className="z-50 ml-auto mt-14 flex h-full w-full flex-col items-center justify-center bg-[#fff] md:mt-0 md:w-4/6 lg:w-4/5">
           {props.children}
